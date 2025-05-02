2caf57beff4b99c66cc9b1637b4bf12f23b53ca6
Mahmud Esad Çıtak <meccaofcalicalwanilyas@gmail.com>
2025-05-02 18:54:47 +0300

    Update main.c
    Added a few comments.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,27 +1,22 @@
+// Standart Libraries
 #include <stdint.h>
 #include <stdio.h>
-
+// User-defined Libraries
 #include "uart.h"
 #include "gpio.h"
 #include "system_timer.h"
 #include "arm_timer.h"
 
 int main() {
-    // İsteğe bağlı çalıştırma kodları
+    // Some functions in BETA
     uart_init();
-    // İsteğe bağlı çalıştırma kodları
     sends_uart("This is Hello World!\nPlease enter the password.\n");
     receives_uart();
     uart_close();
     see_time();
     delay(1000000);
-    see_time();
     delay_arm_timer(3);
-    
     sends_gpio("password");
 
     while(1);
 }
-
-/* Ana fonksiyonun dönüş tipi ve dönüş sonucunda Assemby geçişi hazırlanmalı.
-*/
